37698d0d405dcaecb40f46eeb8404bdbf57f0808
Davide Di Bernardo <dbr.davide@gmail.com>
2019-08-03 19:22:01 +0200

    -added ttl to balloon platforms

---
+++ GMTK2019/Assets/Scripts/Balloon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    [SerializeField]
+    private float m_timeToLive = 10f;
+
+    private Animator m_animator;
+
+    private void Start()
+    {
+        m_animator = GetComponent<Animator>();
+
+        StartCoroutine(CountToDeath());
+    }
+
+    private IEnumerator CountToDeath()
+    {
+        yield return new WaitForSeconds(m_timeToLive);
+
+        Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
--- GMTK2019/Assets/Scripts/KillWhenReach.cs
+++ GMTK2019/Assets/Scripts/KillWhenReach.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class KillWhenReach : MonoBehaviour
 {
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("MovingPlatform"))
         {
